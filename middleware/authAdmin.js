8bc1098ee3c738ab6653caa4b1fc72f1833ebdbd
Az1z_404 <u03062010@gmail.com>
2025-09-02 11:50:59 -0400

    update authorithation

--- middleware/authAdmin.js
+++ middleware/authAdmin.js
@@ -1,24 +1,15 @@
+
+
 const authAdmin = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    if (!authHeader) {
-        return res.status(401).send({ message: 'Authorization header missing' });
-    }
+    const { username, password } = req.headers;
 
-    const [type, credentials] = authHeader.split(' ');
-    if (type !== 'Basic' || !credentials) {
-        return res.status(401).send({ message: 'Invalid authorization format' });
-    }
-
-    const decodedCredentials = Buffer.from(credentials, 'base64').toString('utf-8');
-    const [username, password] = decodedCredentials.split(':');
-
-    const adminName = process.env.ADMIN_NAME;
-    const adminPassword = process.env.ADMIN_PASSWORD;
-
-    if (username === adminName && password === adminPassword) {
-        next();
+    if (
+        username === process.env.ADMIN_USERNAME &&
+        password === process.env.ADMIN_PASSWORD
+    ) {
+        next(); // ✅ Admin ruxsat
     } else {
-        return res.status(403).send({ message: 'Forbidden: Invalid credentials' });
+        return res.status(403).json({ success: false, message: "Access denied" });
     }
 };
 
